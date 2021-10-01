d3d955ff601d11d7a6a4674b193590ce7ffef07e
angeldavid06 <mendozaangel.3243@gmail.com>
2021-09-30 22:32:38 -0500

    2021-09-30 10:32 pm Estilos del proyecto integrador

--- js/fmtor_liberia.js
+++ js/fmtor_liberia.js
@@ -1,10 +1,92 @@
 const btn_menu_toggle = document.getElementById('btn-menu-toggle')
-const btn_subir = document.getElementById('btn-subir')
 
 const menu = document.getElementById('menu')
 const contenido = document.getElementById('contenido')
 
-btn_menu_toggle.addEventListener('click', () => {
+const show_btn = () => {
+    if (document.getElementById("btn-subir")) {
+        const btn_subir = document.getElementById('btn-subir')
+        btn_subir.classList.add('mostrar');
+    }
+}
+
+const hidde_btn = () => {
+    if (document.getElementById("btn-subir")) {
+        const btn_subir = document.getElementById('btn-subir')
+        btn_subir.classList.remove('mostrar');
+    }
+}
+
+const quitar_mas_opciones = () => {
+    const mas_opciones = document.getElementsByClassName('mas_opciones')
+    if (mas_opciones.length > 0) {
+        setTimeout(() => { 
+            mas_opciones[0].classList.remove('mostrar')
+        }, 100);
+        setTimeout(() => { 
+            document.body.removeChild(mas_opciones[0])
+        }, 400);
+    }
+}
+
+const render_mas_opciones = () => {
+    quitar_mas_opciones()
+    const mas_opciones = document.createElement('div')
+    const titulo = document.createElement('titulo')
+    const opciones = document.createElement('opciones')
+
+    mas_opciones.classList.add('mas_opciones')
+    
+    if(document.getElementsByClassName('hidde_menu').length > 0) {
+        mas_opciones.classList.add('w-100')
+    } else {
+        mas_opciones.classList.add('w-80')
+    }
+
+    titulo.innerHTML = '<div class="titulo">'+
+                                '<i class="material-icons" data-quitar="opciones">close</i>'+
+                                '<h3>Opciones a realizar</h3>'+
+                            '</div>'
+                            
+    opciones.innerHTML = '<div class="opciones">'+
+                                '<button class="btn btn-icon btn-transparent" data-id="1">'+
+                                    '<i class="material-icons">delete</i>'+
+                                    ' Eliminar'+
+                                '</button>'+
+                                '<button class="btn btn-icon btn-transparent" data-id="1">'+
+                                    '<i class="material-icons">edit</i>'+
+                                    'Editar'+
+                                    '</button>'+
+                                    '</div>'
+                                    
+    mas_opciones.appendChild(titulo)                        
+    mas_opciones.appendChild(opciones)                        
+    document.body.appendChild(mas_opciones)
+
+    setTimeout(() => { 
+        mas_opciones.classList.add('mostrar')
+    }, 300);
+}
+
+const abrir_cerrar_menu = () => {
     contenido.classList.toggle('hidde_menu')
     menu.classList.toggle('hidde_menu')
+}
+
+contenido.addEventListener('scroll', () => {
+    if (contenido.scrollTop > 150) {
+        show_btn()
+    } else {
+        hidde_btn()
+    }
 });
+
+document.addEventListener('click', (evt) => {
+    if (evt.target.dataset.opciones) {
+        render_mas_opciones(evt.target)
+    } else if (evt.target.dataset.quitar) {
+        quitar_mas_opciones()
+    } else if (evt.target.dataset.menu) {
+        abrir_cerrar_menu()
+    }
+})
